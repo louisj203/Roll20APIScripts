a9a747724b128b79ebc524452f9eccfa44c8544c
Lou-T <71732428+louisj203@users.noreply.github.com>
2020-10-05 06:35:22 -0500

    Update FirstScript.js

--- FirstScript/FirstScript.js
+++ FirstScript/FirstScript.js
@@ -1,1 +1,26 @@
-// HOLD ME
+// Github:   https://github.com/louisj203/Roll20APIScripts/edit/master/FirstScript/FirstScript.js
+// By:       GM Lou-T
+// Contact:  https://app.roll20.net/users/2990909/lou-t
+
+var LogVersionLastUpdate = LogVersionLastUpdate || (function() {
+    'use strict';
+
+    var version = '0.0.1',
+        lastUpdate = 2010050630,
+
+    checkInstall = function() {    
+        log('Install Info: v'+version+' [lastUpdate: '+lastUpdate+']');
+    };
+
+    return {
+        TheReturnName: checkInstall
+    };
+
+}());
+
+on('ready',function() {
+    'use strict';
+
+    TheReturnName.CheckInstall();
+
+});
