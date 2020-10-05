12feba9b367930ffe66b75a83002146a03d04030
Lou-T <71732428+louisj203@users.noreply.github.com>
2020-10-05 06:15:15 -0500

    Update Bloodied_Encumbered.js

--- Bloodied_Encumbered/Bloodied_Encumbered.js
+++ Bloodied_Encumbered/Bloodied_Encumbered.js
@@ -1,5 +1,5 @@
 // Github:   https://github.com/louisj203/Roll20APIScripts/edit/master/Bloodied_Encumbered/Bloodied_Encumbered.js
-// By:       Lou-T, GM Extra Sauce
+// By:       GM Lou-T
 // Contact:  https://app.roll20.net/users/2990909/lou-t
 
 var SayHello = SayHello || (function() {
