fcdb50156b212f375bc25435c2f213e0bab97187
Lou-T <71732428+louisj203@users.noreply.github.com>
2020-10-05 07:29:55 -0500

    Update Bloodied_Encumbered.js

--- Bloodied_Encumbered/Bloodied_Encumbered.js
+++ Bloodied_Encumbered/Bloodied_Encumbered.js
@@ -2,18 +2,18 @@
 // By:       GM Lou-T
 // Contact:  https://app.roll20.net/users/2990909/lou-t
 
-var LogVersionLastUpdate = LogVersionLastUpdate || (function() {
+var Bloodied_Encumbered = Bloodied_Encumbered || (function() {
     'use strict';
 
-    var version = '0.0.2',
-        lastUpdate = 2010050647,
+    var version = '0.0.3',
+        lastUpdate = 2010050730,
 
-    checkInstall = function() {    
+    LogInstall = function() {    
         log('Bloodied_Encumbered Install Info: v'+version+' [lastUpdate: '+lastUpdate+']');
     };
 
     return {
-        TheReturnName: checkInstall
+        B_E_LI: LogInstall
     };
 
 }());
@@ -21,6 +21,6 @@ var LogVersionLastUpdate = LogVersionLastUpdate || (function() {
 on('ready',function() {
     'use strict';
 
-    LogVersionLastUpdate.TheReturnName();
+    Bloodied_Encumbered.B_E_LI();
 
 });
