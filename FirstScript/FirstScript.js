6b03852a4d37a310070419b3018680cb367323a9
Lou-T <71732428+louisj203@users.noreply.github.com>
2020-10-06 04:45:40 -0500

    Update FirstScript.js

--- FirstScript/FirstScript.js
+++ FirstScript/FirstScript.js
@@ -21,7 +21,7 @@ var LogVersionLastUpdate = LogVersionLastUpdate || (function() {
 on('ready',function() {
     'use strict';
 
-    // Okay... what the hell is this?
+    // Okay... what the hell is this? Why is CheckInstall not case sensitive??
     TheReturnName.CheckInstall();
 
 });
