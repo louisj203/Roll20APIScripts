f15bfb0d537f89a2b41edfff63da853affec9bb4
Lou-T <71732428+louisj203@users.noreply.github.com>
2020-10-09 17:50:56 -0500

    Update BloodiedOrDead.js

--- BloodiesOrDead/BloodiedOrDead.js
+++ BloodiesOrDead/BloodiedOrDead.js
@@ -1,10 +1,20 @@
 on("change:graphic", function(obj) {
     if(obj.get("bar1_max") === "") return;
-    
-    //RESET ALL TINTS / STATUSMARKERS BEFORE DETERMINING NEW ONES
-    obj.set({
-        
-    )};
+
+    if(obj.get("bar1_value") <= 0) {
+        //Set dead status marker and remove bloodied status marker and any tints, then return.
+        obj.set({
+            tint_color: "transparent"
+            status_bleeding: false
+            status_dead: true
+        });
+    return;
+    }
+    else {
+      obj.set({
+        status_dead: false
+      });
+    }
     
     if(obj.get("bar1_value") <= obj.get("bar1_max") / 2) {
         obj.set({
@@ -16,15 +26,5 @@ on("change:graphic", function(obj) {
             status_redmarker: false
         })
     }
-
-    if(obj.get("bar1_value") <= 0) {
-      obj.set({
-         status_dead: true
-      });
-    }
-    else {
-      obj.set({
-        status_dead: false
-      });
-    }
+    
 });
