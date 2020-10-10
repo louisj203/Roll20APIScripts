ec62d7e1128d59bceba2f341fcef88269040ea54
Lou-T <71732428+louisj203@users.noreply.github.com>
2020-10-09 20:30:40 -0500

    Update BloodiedOrDead.js

--- BloodiedOrDead/BloodiedOrDead.js
+++ BloodiedOrDead/BloodiedOrDead.js
@@ -9,19 +9,19 @@ on("change:graphic", function(obj) {
     if(obj.get("bar1_max") === "") return;
     
     var IsNamedCharacter = false
-    if(obj.get("??TOKENTYPE" == ??) {
-        IsNamedCharacter = true
-    }
+    //if(obj.get("??TOKENTYPE" == ??) {
+    //    IsNamedCharacter = true
+    //}
 
     if(obj.get("bar1_value") <= 0) {
-        //Set dead status marker and remove bloodied status marker and any tints, send dying message (if is a player character), then return.
-        //  Object Set acceptable syntax: obj.set("property", newvalue) or obj.set({property: newvalue, property2: newvalue2})
+        //Set dead status marker and remove bloodied status marker and any tints, send dying message (if is a named character), then return.
+        //  ** Object Set acceptable syntax: obj.set("property", newvalue) or obj.set({property: newvalue, property2: newvalue2}) **
         obj.set({
             tint_color: "transparent",
             status_bleeding: false,
             status_dead: true
         });
-        if(obj.get("IsPlayer??") {
+        if(IsNamedCharacter) {
         //Send a message to everyone of the players dying status
                        
         }
@@ -32,34 +32,34 @@ on("change:graphic", function(obj) {
     }
     
     //Determine current hitpoint ration "bar1_value" / "bar1_max"
-    var hpRatio = obj.get("bar1_value") / obj.get("bar1_max);
+    var hpRatio = (obj.get("bar1_value") / obj.get("bar1_max));
     
     if(hpRatio <= 0.25) {   //Gravely wounded    
         obj.set({
-            tint_color: "RED!!!"
+            tint_color: "RED"
             status_bleeding: true
         });
-        if(obj.get("IsPlayer??") {
+        if(IsNamedCharacter) {
         //Send a message to everyone of the players gravely wounded status
                        
         }
     }
     elseif(hpRation <= 0.5) {   //Bloodied
         obj.set({
-            tint_color: "YELLOW!!"
+            tint_color: "YELLOW"
             status_bleeding: true
         });
-        if(obj.get("IsPlayer??") {
+        if(IsNamedCharacter) {
         //Send a message to everyone of the players bloodied status
                        
         }        
     }
     elseif(hpRatio <= 0.75 {    //Injured
         obj.set({
-            tint_color: "YELLOW!!"
+            tint_color: "YELLOW"
             status_bleeding: false
         });
-        if(obj.get("IsPlayer??") {
+        if(IsNamedCharacter) {
         //Send a message to everyone of the players injured status
                        
         }                 
