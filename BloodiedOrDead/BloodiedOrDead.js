3357857f8182f4cc5623a13985b82b69ed075eb8
Lou-T <71732428+louisj203@users.noreply.github.com>
2020-10-09 22:01:09 -0500

    Update BloodiedOrDead.js

--- BloodiedOrDead/BloodiedOrDead.js
+++ BloodiedOrDead/BloodiedOrDead.js
@@ -6,13 +6,14 @@
 const version = "0.0.1",
       lastUpdate = 2010091600;
 
+var IsNamedCharacter = true;
+
 on("change:graphic", function(obj) {
       
     if(obj.get("bar1_max") == "" || obj.get("bar1_value" == "")) {
         return;
     }
     
-    var IsNamedCharacter = false;
     //if(obj.get("??TOKENTYPE" == ??) {
     //    IsNamedCharacter = true
     //}
